cac3abd1d632cc4a2921c69dd691be307d77ad6f
nobody <nobody@example.com>
2023-03-07 22:38:46 +0800

    update

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 with open("README.md", "r", encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name="cloudflare-ddns",
     version="1.0.0",
     license='MIT',
     description="A DDNS service for Cloudflare domain name",
+    author='Behappy0',
     url='https://github.com/Behappy0/Cloudflare-DDNS',
     packages=['cloudflare_ddns'],
     package_data={
