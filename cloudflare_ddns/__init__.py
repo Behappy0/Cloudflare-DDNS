47973e5efaae8cb190b9e622b0dac5117b47e170
nobody <nobody@example.com>
2024-04-04 12:32:04 +0800

    update

--- cloudflare_ddns/__init__.py
+++ cloudflare_ddns/__init__.py
@@ -3,6 +3,6 @@
 from cloudflare_ddns.service import CloudflareDDNS
 
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 __all__ = ["CloudflareDDNS", "__version__"]
--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
+import os
+import re
+import codecs
 from setuptools import setup
-from cloudflare_ddns import __version__ as package_version
 
 
 with open("README.md", "r", encoding='utf-8') as f:
     long_description = f.read()
 
+
+def read_file(filename, encoding="utf8"):
+    with codecs.open(filename, encoding=encoding) as fd:
+        return fd.read()
+
+
+package_name = "cloudflare_ddns"
+here = os.path.abspath(os.path.dirname(__file__))
+init_fn = os.path.join(here, package_name, "__init__.py")
+meta = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", read_file(init_fn)))
+
+
 setup(
     name="cloudflare-ddns",
-    version=package_version,
+    version=meta["version"],
     license='MIT',
     description="A DDNS service for Cloudflare domain name",
     author='Behappy0',
     url='https://github.com/Behappy0/Cloudflare-DDNS',
-    packages=['cloudflare_ddns'],
+    packages=[package_name],
     include_package_data=True,
     install_requires=[],
     python_requires=">=3.9",
